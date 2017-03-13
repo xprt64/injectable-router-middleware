113c9aeea1b3a261b71fa00237774bd6e2c1b361
Constantin GALBENU <gica.galbenu@gmail.com>
2017-03-13 13:23:18 +0200

    initial commit

---
+++ src/Gica/ParameterInjecterMiddleware/ParameterInjecter.php
@@ -0,0 +1,243 @@
+<?php
+/******************************************************************************
+ * Copyright (c) 2017 Constantin Galbenu <gica.galbenu@gmail.com>             *
+ ******************************************************************************/
+
+namespace Gica\ParameterInjecterMiddleware;
+
+use Gica\CodeAnalysis\Shared\FqnResolver;
+use Psr\Container\ContainerInterface;
+use Psr\Http\Message\ResponseInterface;
+use Psr\Http\Message\ServerRequestInterface;
+use Zend\Expressive\Router\RouteResult;
+
+class ParameterInjecter
+{
+
+    /**
+     * @var ContainerInterface
+     */
+    private $container;
+
+    private $variables;
+
+    /** @var \ReflectionClass */
+    private $middlewareClass;
+
+    public function __construct(
+        ContainerInterface $container
+    )
+    {
+        $this->container = $container;
+    }
+
+    public function __invoke(ServerRequestInterface $request, ResponseInterface $response, callable $next = null)
+    {
+        /** @var RouteResult $matchedRoute */
+        $matchedRoute = $request->getAttribute(RouteResult::class);
+
+        $className = $matchedRoute->getMatchedMiddleware();
+
+        if (!$className) {
+            return $next($request, $request);
+        }
+
+        $this->middlewareClass = new \ReflectionClass($className);
+
+        if (!$this->middlewareClass->hasMethod('__invoke')) {
+            return $next($request, $request);
+        }
+
+        $invoke = $this->middlewareClass->getMethod('__invoke');
+
+        $this->variables = [
+            'request'  => $request,
+            'response' => $response,
+            'next'     => $next,
+        ];
+
+        $get = $request->getQueryParams();
+
+        $callArguments = [];
+
+        foreach ($invoke->getParameters() as $parameter) {
+            $rawValue = $this->getValue($request, $parameter, $get);
+
+            //echo $parameter->getName() . ' ' . (string)$parameter->getType() . ' ' . print_r($rawValue, true) . (is_null($rawValue) ? ' (NULL)' : '') . "\n\n";
+
+            $callArguments[] = $this->parseParameter($rawValue, $parameter, $invoke);
+        }
+
+
+//        foreach($callArguments as $i => $argument)
+//        {
+//            echo $i . "." . (null !== $argument ? get_class($argument) : 'NULL') . "\n\n";
+//        }
+//
+//        echo $className;
+//
+//        die();
+
+        unset($this->variables);
+        unset($this->middlewareClass);
+
+        return call_user_func_array([$this->container->get($className), '__invoke'], $callArguments);
+    }
+
+    private function parseParameter($rawValue, \ReflectionParameter $parameter, \ReflectionMethod $method)
+    {
+        $name = $parameter->getName();
+        if (isset($this->variables[$name])) {
+            return $this->variables[$name];
+        }
+
+        if (!$parameter->hasType()) {
+            return $rawValue;
+        } else {
+            if (null === $rawValue) {
+                if (!$parameter->getType()->allowsNull()) {
+                    if ($parameter->isDefaultValueAvailable()) {
+                        return $parameter->getDefaultValue();
+                    } else {
+                        throw new \Exception("Parameter $name must not be null");
+                    }
+                } else {
+                    return null;
+                }
+            }
+
+            if ('array' == $parameter->getType()) {
+                return $this->hydrateArrayValue($rawValue, $parameter, $method);
+            } else if ($this->isScalar((string)$parameter->getType())) {
+                return $this->hydrateBuiltinValue($rawValue, $parameter->getType());
+            } else {
+                return $this->hydrateCustomValue($rawValue, $parameter->getClass(), $parameter->name);
+            }
+        }
+    }
+
+    private function isScalar(string $shortType): bool
+    {
+        return in_array($shortType, ['bool', 'string', 'int', 'boolean', 'null', 'float', 'double']);
+    }
+
+    private function hydrateBuiltinValue($rawValue, string $type)
+    {
+
+        switch ($type) {
+            case 'string':
+                return strval($rawValue);
+            case 'int':
+                return intval($rawValue, 10);
+            case 'double':
+            case 'float':
+                return floatval($rawValue);
+            case 'bool':
+            case 'boolean':
+                return boolval($rawValue);
+
+            default:
+                return $rawValue;
+        }
+    }
+
+    private function hydrateCustomValue($rawValue, \ReflectionClass $reflectionClass, string $parameterName)
+    {
+        $object = $this->tryFromString($reflectionClass, $rawValue);
+
+        if (null !== $object) {
+            return $object;
+        }
+
+        $object = $this->tryFromPrimitive($reflectionClass, $rawValue);
+
+        if (null !== $object) {
+            return $object;
+        }
+
+
+        throw new \Exception("unkown custom value for {$parameterName}@{$reflectionClass->name}");
+    }
+
+
+    private function tryFromString(\ReflectionClass $reflectionClass, $document)
+    {
+        if ((is_string($document) || is_callable([$document, '__toString']))) {
+            if ($reflectionClass->hasMethod('fromString')) {
+                $method = $reflectionClass->getMethod('fromString');
+
+                if ($method->isStatic()) {
+                    return $method->invoke(null, (string)$document);
+                }
+            }
+        }
+
+        return null;
+    }
+
+    private function tryFromPrimitive(\ReflectionClass $reflectionClass, $document)
+    {
+        if (is_scalar($document) || is_array($document)) {
+            if (is_callable([$reflectionClass->getName(), 'fromPrimitive'])) {
+                return call_user_func([$reflectionClass->getName(), 'fromPrimitive'], $document);
+            }
+        }
+
+        return null;
+    }
+
+    private function hydrateArrayValue($rawValue, \ReflectionParameter $parameter, \ReflectionMethod $method)
+    {
+        $class = $this->detectClassNameFromPropertyComment($parameter, $method);
+
+        $rawValues = is_array($rawValue) ? $rawValue : explode('|', $rawValue);
+
+        return array_map(function ($rawValue) use ($parameter, $class) {
+            if ($this->isScalar($class)) {
+                return $this->hydrateBuiltinValue($rawValue, $class);
+            } else {
+                return $this->hydrateCustomValue($rawValue, new \ReflectionClass($class), $parameter->name);
+            }
+        }, $rawValues);
+    }
+
+    private function parseTypeFromPropertyVarDoc(string $parameterName, \ReflectionMethod $method)
+    {
+        if (!preg_match('#\@param\s+(?P<shortType>[\\\\a-z0-9_\]\[]+)\s+\$' . $parameterName . '#ims', $method->getDocComment(), $m)) {
+            throw new \Exception("Could not detect type from vardoc for parameter {$parameterName}");
+        }
+        return $m['shortType'];
+    }
+
+    private function detectClassNameFromPropertyComment(\ReflectionParameter $parameter, \ReflectionMethod $method)
+    {
+        $shortType = $this->parseTypeFromPropertyVarDoc($parameter->name, $method);
+
+        $shortType = rtrim($shortType, '[]');
+
+        if ('array' === $shortType) {
+            return null;
+        }
+
+        if ('\\' == $shortType[0]) {
+            return ltrim($shortType, '\\');
+        }
+
+        if ($this->isScalar($shortType)) {
+            return $shortType;
+        }
+
+        return ltrim($this->resolveShortClassName($shortType, $this->middlewareClass), '\\');
+    }
+
+    private function resolveShortClassName($shortName, \ReflectionClass $contextClass)
+    {
+        return (new FqnResolver())->resolveShortClassName($shortName, $contextClass);
+
+    }
+
+    private function getValue(ServerRequestInterface $request, \ReflectionParameter $parameter, $get)
+    {
+        return (null !== $request->getAttribute($parameter->name)) ? $request->getAttribute($parameter->name) : @$get[$parameter->name];
+    }
+}
---
+++ tests/Gica/ParameterInjecterMiddleware/ParameterExtracterMiddlewareTest.php
@@ -0,0 +1,146 @@
+<?php
+/******************************************************************************
+ * Copyright (c) 2017 Constantin Galbenu <gica.galbenu@gmail.com>             *
+ ******************************************************************************/
+
+namespace tests\Gica\ParameterInjecterMiddleware;
+
+use Gica\ParameterInjecterMiddleware\ParameterInjecter;
+use Psr\Container\ContainerInterface;
+use Psr\Http\Message\ResponseInterface;
+use Psr\Http\Message\ServerRequestInterface;
+use Zend\Expressive\Router\RouteResult;
+
+class ParameterExtracterMiddlewareTest extends \PHPUnit_Framework_TestCase
+{
+
+    public function test___invoke()
+    {
+        $request = $this->getMockBuilder(ServerRequestInterface::class)
+            //   ->setMethods(['getAttribute', 'getQueryParams'])
+            ->getMock();
+
+        $matchedRoute = $this->getMockBuilder(RouteResult::class)
+            ->disableOriginalConstructor()
+            ->setMethods(['getMatchedMiddleware'])
+            ->getMock();
+
+        $matchedRoute->method('getMatchedMiddleware')
+            ->willReturn(WebPage::class);
+
+        $map = [
+            RouteResult::class => $matchedRoute,
+            'integerAttribute' => 123,
+            'floatAttribute'   => 123.456,
+            'stringAttribute'  => 'abc',
+            'integers'         => '1|2|3',
+        ];
+
+        $request->method('getAttribute')
+            ->will($this->returnCallback(function ($what) use ($map) {
+                return isset($map[$what]) ? $map[$what] : null;
+            }));
+
+        $get = [
+            'someQueryParameter' => 'someQueryParameterValue',
+            'customAttribute'    => 'customAttributeValue',
+            'integers'           => '100|101|102',//this should be ignored
+        ];
+
+        $request->method('getQueryParams')
+            ->willReturn($get);
+
+        $response = $this->getMockBuilder(ResponseInterface::class)
+            ->getMock();
+
+        $called = $this->getMockBuilder(WebPage::class)
+            ->getMock();
+
+        $called->expects($this->once())
+            ->method('__invoke')
+            ->with(
+                $this->identicalTo(123),
+                $this->identicalTo(123.456),
+                $this->identicalTo('abc'),
+                $this->isInstanceOf(CustomAttribute::class),
+                $this->identicalTo($request),
+                $this->identicalTo('someQueryParameterValue'),
+                $this->isNull(),
+                $this->equalTo([1, 2, 3])
+            );
+
+
+        $container = $this->getMockBuilder(ContainerInterface::class)
+            ->getMock();
+        $container->method('get')
+            ->with($this->equalTo(WebPage::class))
+            ->willReturn($called);
+
+        /** @var ContainerInterface $container */
+        $sut = new ParameterInjecter($container);
+
+        /** @var ServerRequestInterface $request */
+        /** @var ResponseInterface $response */
+
+        $sut->__invoke($request, $response);
+    }
+}
+
+class WebPage
+{
+    /**
+     * @param int $integerAttribute
+     * @param float $floatAttribute
+     * @param string $stringAttribute
+     * @param CustomAttribute $customAttribute
+     * @param ServerRequestInterface $request
+     * @param string $someQueryParameter
+     * @param string|null $someNullQueryParameter
+     * @param int[] $integers
+     */
+    public function __invoke(
+        int $integerAttribute,
+        float $floatAttribute,
+        string $stringAttribute,
+        CustomAttribute $customAttribute,
+        ServerRequestInterface $request,
+        string $someQueryParameter,
+        string $someNullQueryParameter = null,
+        array $integers
+    )
+    {
+
+    }
+}
+
+class CustomAttribute
+{
+
+    private $value;
+
+    public function __construct(
+        $value
+    )
+    {
+        if (false === stripos($value, '-custom-attribute')) {
+            throw new \Exception("Invalid Custom value");
+        }
+
+        $this->value = $value;
+    }
+
+    public static function makeValue($str): string
+    {
+        return $str . '-custom-attribute';
+    }
+
+    public function getValue()
+    {
+        return $this->value;
+    }
+
+    public static function fromString($str)
+    {
+        return new self(self::makeValue($str));
+    }
+}
